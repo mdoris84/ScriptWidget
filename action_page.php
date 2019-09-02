63cf9582a4952cd2c526a73041a276a613ba3ffe
Muhammad Doris <53292797+mdoris84@users.noreply.github.com>
2019-09-02 11:00:01 +0700

    add files

---
+++ action_page.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<!-- Show success message -->
+<h2>You will receive a call soon</h2>
+<!-- Back to previous page-->
+<META HTTP-EQUIV="Refresh" CONTENT="1;URL=index.html">
+</body>
+</html> 
